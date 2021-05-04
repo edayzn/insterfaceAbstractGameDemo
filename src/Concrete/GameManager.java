5607f6b8d21a20cb2b1ab72da85ac3c1a666a5d6
Eda <eda.yzn5@gmail.com>
2021-05-04 17:40:27 +0300

    Homework

--- src/Concrete/GameManager.java
+++ src/Concrete/GameManager.java
@@ -1,9 +1,8 @@
 package Concrete;
 
 import Abstract.GameService;
-import Entities.Campaign;
 import Entities.Game;
-import Entities.Gamer;
+
 
 public class GameManager implements GameService{
 
@@ -11,14 +10,12 @@ public class GameManager implements GameService{
 	public void add(Game game) {
 		System.out.println("Saved " + game.getGameName());
 
-		
 	}
 
 	@Override
 	public void update(Game game) {
 		System.out.println("Updated " + game.getGameName());
 
-		
 	}
 
 	@Override
